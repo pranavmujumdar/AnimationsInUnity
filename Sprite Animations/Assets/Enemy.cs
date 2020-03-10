da6869a84a0094c49dee031e2d1f8aa3bf7a47f6
Pranav Mujumdar <psmujumdar@gmail.com>
2020-03-10 13:00:36 -0400

    added the wait logic to the script

--- Sprite Animations/Assets/Enemy.cs
+++ Sprite Animations/Assets/Enemy.cs
@@ -6,7 +6,14 @@ public class Enemy : MonoBehaviour
 {
     public Animator animator;
     public int health = 100;
-
+    private float speed = 2f;
+    public Transform groundDetection;
+    private bool movingRight = true;
+    public Transform PlayerDetectionBehind;
+    public Transform PlayerDetectionFront;
+    public float playerBehindDistance = 1f;
+    public float playerFrontDistance = 2f;
+    
     // public GameObject deathEffect;
     // Start is called before the first frame update
 
@@ -25,7 +32,94 @@ public class Enemy : MonoBehaviour
         {
             animator.SetBool("isDead", true);
         }
+        
+
+        if(movingRight == true)
+        {
+
+            //Debug.Log("Moving Rigt");
+            //RaycastHit2D playerInfo = Physics2D.Raycast(PlayerDetectionBehind.position, Vector2.left, playerBehindDistance);
+            RaycastHit2D playerInfoFront = Physics2D.Raycast(PlayerDetectionFront.position, Vector2.right, playerFrontDistance);
+            //Debug.DrawRay(PlayerDetectionBehind.position, Vector2.left * playerBehindDistance, Color.red);
+            Debug.DrawRay(PlayerDetectionFront.position, Vector2.right * playerFrontDistance, Color.magenta);
+            if (playerInfoFront.collider == true)
+            {
+                //Debug.Log(playerInfo.collider.tag);
+                if(playerInfoFront.collider.tag == "Player")
+                {
+                    Debug.Log("Player");
+                }
+                //Debug.Log("Player in range!");
+            }
+        }
+        else
+        {
+            //Debug.Log("moving Left");
+
+            RaycastHit2D playerInfoFront = Physics2D.Raycast(PlayerDetectionFront.position, Vector2.left, playerFrontDistance);
+            Debug.DrawRay(PlayerDetectionFront.position, Vector2.left * playerFrontDistance, Color.green);
+
+            if (playerInfoFront.collider == true)
+            {
+                if (playerInfoFront.collider.tag == "Player")
+                {
+                    Debug.Log("Player");
+                }
+                //Debug.Log("Player in range!");
+            }
+        }
+
+
+
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, 1f);
+
+        //Debug.DrawRay(PlayerDetectionLeft.position, Vector2.left * 1f, Color.cyan);
+
+
+        Invoke("moveEnemy", 2f);
+        if (groundInfo.collider == false)
+        {
+            if (movingRight == true)
+            {
+                animator.SetFloat("Speed", 0);
+                CancelInvoke("moveEnemy");
+                transform.Rotate(0f, 180f, 0f);
+                movingRight = false;
+            }
+            else
+            {
+                animator.SetFloat("Speed", 0);
+                CancelInvoke("moveEnemy");
+                transform.Rotate(0f, 180f, 0f);
+                movingRight = true;
+            }
+        }
     }
+
+    IEnumerator rotateEnemy()
+    {
+        yield return new WaitForSeconds(2.0f);
+        transform.Rotate(0f, 180f, 0f);
+    }
+    /*IEnumerator moveEnemy()
+    {
+        yield return new WaitForSeconds(0.0f);
+        animator.SetFloat("Speed", 2);
+        transform.Rotate(0f, 180f, 0f);
+    }*/
+    
+    public void stopEnemy()
+    {
+        animator.SetFloat("Speed", 0);
+        transform.Translate(Vector2.zero);
+    }
+    public void moveEnemy()
+    {
+        animator.SetFloat("Speed", 2);
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
     public void Die()
     {
         Destroy(gameObject,2f);
