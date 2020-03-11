d85e928148c2b399e6ebf48cb1d2b2cfd4409edd
Pranav Mujumdar <psmujumdar@gmail.com>
2020-03-11 02:14:11 -0400

    Button script for next and restart

---
+++ Sprite Animations/Assets/ButtonScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ButtonScript : MonoBehaviour
+{
+    public void nextLevel()
+    {
+        ScoreScript.scoreValue = 0;
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+    }
+}
