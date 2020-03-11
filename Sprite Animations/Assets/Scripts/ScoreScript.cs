67c4c256cff1bfccba0541c2792ec1d29c45af17
Pranav Mujumdar <psmujumdar@gmail.com>
2020-03-11 02:16:39 -0400

    Added script to maintain the score

---
+++ Sprite Animations/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreScript : MonoBehaviour
+{
+    public static int scoreValue = 0;
+    public TextMeshProUGUI ScoreText; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreText.text = "Score: " + scoreValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
